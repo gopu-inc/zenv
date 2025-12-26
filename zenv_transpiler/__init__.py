a7ae7ad5e379ad5a70829313198c41b470b5803f
Mauricio-100 <ceoseshell@gmail.com>
2025-12-26 23:46:55 +0100

    Create __init__.py

---
+++ zenv_transpiler/__init__.py
@@ -0,0 +1,4 @@
+# zenv_transpiler/__init__.py
+from .transpiler import transpile_string, transpile_file, ZvSyntaxError, BRAND
+
+__all__ = ["transpile_string", "transpile_file", "ZvSyntaxError", "BRAND"]
