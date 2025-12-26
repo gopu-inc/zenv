26b0239cfa1aa36942b2ff01b04d43039a762b9f
Mauricio-100 <ceoseshell@gmail.com>
2025-12-27 00:52:10 +0100

    Update __init__.py

--- zenv_transpiler/__init__.py
+++ zenv_transpiler/__init__.py
@@ -1,4 +1,36 @@
 # zenv_transpiler/__init__.py
-from .transpiler import transpile_string, transpile_file, ZvSyntaxError, BRAND
+"""
+Zenv Transpiler - Un transpileur de .zv/.zenv vers Python
+"""
 
-__all__ = ["transpile_string", "transpile_file", "ZvSyntaxError", "BRAND"]
+from .transpiler import (
+    transpile_string,
+    transpile_file,
+    transpile_directory,
+    validate_zv_code,
+    get_zv_version,
+    get_supported_features,
+    ZvSyntaxError,
+    BRAND
+)
+
+from .cli import main
+
+__version__ = get_zv_version()
+__author__ = "Votre Nom"
+__description__ = "Transpileur Zenv vers Python"
+
+__all__ = [
+    'transpile_string',
+    'transpile_file',
+    'transpile_directory',
+    'validate_zv_code',
+    'get_zv_version',
+    'get_supported_features',
+    'ZvSyntaxError',
+    'BRAND',
+    'main',
+    '__version__',
+    '__author__',
+    '__description__'
+]
