c39ecc46c4ffbba83bd3a2156d8d8d37495ad95c
Mauricio-100 <ceoseshell@gmail.com>
2025-12-27 17:29:15 +0100

    Update __init__.py

--- znv/__init__.py
+++ znv/__init__.py
@@ -1,1 +1,23 @@
 
+"""
+Zenv Runtime - Écosystème complet de développement
+"""
+
+__version__ = "0.1.0"
+__author__ = "Zenv Team"
+__license__ = "MIT"
+
+from .runtime import ZenvVirtualMachine, ZenvREPL
+from .package_manager import ZenvPackageManager
+from .virtualenv import ZenvVirtualEnv
+from .builder import ZenvBuilder
+from .cache import ZenvCache
+
+__all__ = [
+    'ZenvVirtualMachine',
+    'ZenvREPL', 
+    'ZenvPackageManager',
+    'ZenvVirtualEnv',
+    'ZenvBuilder',
+    'ZenvCache',
+]
