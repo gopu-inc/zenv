5c6f34498996095ff500cecd74a06fda97ffeb31
Mauricio-100 <ceoseshell@gmail.com>
2025-12-28 17:32:29 +0100

    Update __main__.py

--- zenv/__main__.py
+++ zenv/__main__.py
@@ -1,19 +1,14 @@
 #!/usr/bin/env python3
 """
-Point d'entrée principal de l'écosystème Zenv
+Zenv Language CLI Entry Point
 """
 
 import sys
-import os
-
-# Ajouter le chemin courant
-sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
-
-from zenv.command.com import ZenvCLI
+from .cli import ZenvCLI
 
 def main():
     cli = ZenvCLI()
-    return cli.run(sys.argv[1:])
+    sys.exit(cli.run(sys.argv[1:]))
 
 if __name__ == "__main__":
-    sys.exit(main())
+    main()
