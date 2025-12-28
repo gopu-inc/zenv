b479a025ed9bdb12f1d1946b3d34bc7ebcc1d431
Mauricio-100 <ceoseshell@gmail.com>
2025-12-28 13:56:35 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ here = pathlib.Path(__file__).parent.resolve()
 long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
-    name="zenv",
+    name="zenv-lang",
     version="0.1.0",
     description="Écosystème Zenv - Runtime, CLI et Package Manager",
     long_description=long_description,
